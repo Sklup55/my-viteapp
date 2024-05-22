006bd1d929cffcc80373f708e579777b9c17a28f
Sklup55 <ksundararajan@skillup.tech>
2024-05-22 08:46:12 -0400

    ex 3 majorly done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This allows connections from any IP address
+    port: 5173, // Ensure this matches the port your virtual lab environment uses
+    hmr: {
+      protocol: 'wss', // Use WebSocket Secure for HMR
+      host: 'ksundararaja-5173.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', // Your virtual environment host
+      clientPort: 443, // Use the default port for HTTPS
+    },
+  },
+  base: '', // Ensure Vite uses relative paths
 })
